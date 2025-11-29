5239f3fd6b3d7f6e001ffcd19a4b157cb73e84d6
Vitor <vitor2071@gmail.com>
2025-11-29 10:18:15 -0300

    feat: implementa desafio 08 nivel 02

---
+++ nivel 2/08.swift
@@ -0,0 +1,20 @@
+// Usando o enum ProductDetailState do artigo (loading, loaded, error), adicione um método shouldShowActivityIndicator: Bool que retorna true se o estado for .loading e false para os outros casos.
+
+
+enum ProductDetailState {
+  case loading
+  case loaded
+  case error
+
+  func shouldShowActivityIndicator() -> Bool {
+    switch self {
+      case .loading:
+        return true
+      case .loaded, .false: 
+        return false
+    }
+  }
+}
+
+let loading = ProductDetailState.loading
+print(loading.shouldShowActivityIndicator())
